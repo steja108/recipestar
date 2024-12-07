c44dc3bc2423565440e9b3ad91216d672ffdf331
steja976 <steja976@gmail.com>
2024-12-06 22:54:46 -0500

    fifth commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,134 +1,118 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Icons for password visibility
-import { FaFacebook, FaGoogle, FaApple } from 'react-icons/fa';
+import { FaEye, FaEyeSlash, FaFacebook, FaGoogle, FaApple } from 'react-icons/fa'; // Consolidated icon imports
 import GoogleLoginButton from '../components/GoogleLoginButton';
-import "../css/SignUp.css"; // Ensure the CSS path is correct
+import '../css/SignUp.css'; // Ensure the CSS path is correct
 import recipe_star_logo from '../images/recipe_star_logo.png';
 
-class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      password: '',
-      error: '',
-      success: '',
-      showPassword: false,
-    };
-  }
+const Login = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-  navigate = useNavigate();
+  const navigate = useNavigate();
 
-  navigateToSignUp = () => {
+  const navigateToHome = () => {
+    navigate('/');
+  };
+
+  const navigateToSignUp = () => {
     navigate('/signup');
   };
 
-  navigateToLogin = () => {
+  const navigateToLogin = () => {
     navigate('/login');
   };
 
-  navigateToRecipes = () => {
-    navigate('/recipes');
-  };
-
-  navigateToAboutus = () => {
-    navigate('/aboutus');
-  };
-
-  navigateToHome = () => {
-    navigate('/');
-  };
-  // Handle input changes
-  handleInputChange = (event) => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
-    this.setState({
+    setFormData((prevData) => ({
+      ...prevData,
       [name]: value,
-    });
-  };
-
-  // Handle password visibility toggle
-  togglePasswordVisibility = () => {
-    this.setState((prevState) => ({
-      showPassword: !prevState.showPassword,
     }));
   };
 
-  // Handle form submission
-  handleSubmit = async (event) => {
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const { name, email, password } = this.state;
+    const { name, email, password } = formData;
 
     try {
       const response = await axios.post('http://localhost:5001/api/users/register', { name, email, password });
-      this.setState({ success: response.data.message, error: '' });
-      // Redirect to login page or another page
+      setSuccess(response.data.message);
+      setError('');
     } catch (error) {
-      this.setState({
-        error: error.response ? error.response.data.message : 'An error occurred',
-        success: '',
-      });
+      setError(error.response ? error.response.data.message : 'An error occurred');
+      setSuccess('');
     }
   };
 
-  render() {
-    const { name, email, password, error, success, showPassword } = this.state;
-
-    return (
-      <div className="signup-container">
-        {/* Left Panel */}
-        <div className="left-panel">
-          <div className="logo">
-            <a onClick={this.navigateToHome}>
+  return (
+    <div className="signup-container">
+      {/* Left Panel */}
+      <div className="left-panel">
+        <div className="logo">
+          <a onClick={() => navigate('/')}>
             <img
               src={recipe_star_logo}
-              alt="InsideBox Logo"
+              alt="Recipe Star Logo"
               className="logo-img1"
             />
-            </a>
-          </div>
-          <h2 className="signup-heading">Login to Recipe Star</h2>
-          {error && <p className="error-text">{error}</p>}
-          {success && <p className="success-text">{success}</p>}
-          <form onSubmit={this.handleSubmit} className="signup-form">
+          </a>
+        </div>
+        <h2 className="signup-heading">Sign up to Recipe Star</h2>
+        {error && <p className="error-text">{error}</p>}
+        {success && <p className="success-text">{success}</p>}
+
+        <form onSubmit={handleSubmit} className="signup-form">
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            className="input-field"
+            placeholder="Email address"
+          />
+          <div className="password-wrapper">
             <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleInputChange}
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
               className="input-field"
-              placeholder="Email address"
+              placeholder="Password"
             />
-            <div className="password-wrapper">
-              <input
-                type={showPassword ? 'text' : 'password'}
-                name="password"
-                value={password}
-                onChange={this.handleInputChange}
-                className="input-field"
-                placeholder="Password"
-              />
-              <span className="password-icon" onClick={this.togglePasswordVisibility}>
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
-              </span>
-            </div>
-            <button type="submit" className="signup-button">
-              Login
-            </button>
-          </form>
-          <p className="signin-text">
-            Don't have an account? <a onClick={this.navigateToSignUp} className="signin-link">Sign Up</a>
-          </p>
-        </div>
-
-        {/* Right Panel */}
-        <div className="right-panel">
-        </div>
+            <span className="password-icon" onClick={togglePasswordVisibility}>
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
+          </div>
+          <button type="submit" className="signup-button">
+            Sign up
+          </button>
+        </form>
+        <p className="signin-text">
+          Don't Have an account?{' '}
+          <a onClick={navigateToSignUp} className="signin-link">
+            Sign Up
+          </a>
+        </p>
       </div>
-    );
-  }
-}
 
-export default Signup;
+      {/* Right Panel */}
+      <div className="right-panel">
+        {/* Add content here if needed */}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,141 +1,125 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Icons for password visibility
-import { FaFacebook, FaGoogle, FaApple } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { FaEye, FaEyeSlash, FaFacebook, FaGoogle, FaApple } from 'react-icons/fa'; // Consolidated icon imports
 import GoogleLoginButton from '../components/GoogleLoginButton';
-import "../css/SignUp.css"; // Ensure the CSS path is correct
+import '../css/SignUp.css'; // Ensure the CSS path is correct
 import recipe_star_logo from '../images/recipe_star_logo.png';
 
-class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      password: '',
-      error: '',
-      success: '',
-      showPassword: false,
-    };
-  }
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-  navigate = useNavigate();
+  const navigate = useNavigate();
+  const navigateToHome = () => {
+    navigate('/');
+  };
 
-  navigateToSignUp = () => {
+  const navigateToSignUp = () => {
     navigate('/signup');
   };
 
-  navigateToLogin = () => {
+  const navigateToLogin = () => {
     navigate('/login');
   };
 
-  navigateToRecipes = () => {
-    navigate('/recipes');
-  };
-
-  navigateToAboutus = () => {
-    navigate('/aboutus');
-  };
-  navigateToHome = () => {
-    navigate('/');
-  };
-  // Handle input changes
-  handleInputChange = (event) => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
-    this.setState({
+    setFormData((prevData) => ({
+      ...prevData,
       [name]: value,
-    });
-  };
-
-  // Handle password visibility toggle
-  togglePasswordVisibility = () => {
-    this.setState((prevState) => ({
-      showPassword: !prevState.showPassword,
     }));
   };
 
-  // Handle form submission
-  handleSubmit = async (event) => {
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const { name, email, password } = this.state;
+    const { name, email, password } = formData;
 
     try {
       const response = await axios.post('http://localhost:5001/api/users/register', { name, email, password });
-      this.setState({ success: response.data.message, error: '' });
-      // Redirect to login page or another page
+      setSuccess(response.data.message);
+      setError('');
     } catch (error) {
-      this.setState({
-        error: error.response ? error.response.data.message : 'An error occurred',
-        success: '',
-      });
+      setError(error.response ? error.response.data.message : 'An error occurred');
+      setSuccess('');
     }
   };
 
-  render() {
-    const { name, email, password, error, success, showPassword } = this.state;
-
-    return (
-      <div className="signup-container">
-        {/* Left Panel */}
-        <div className="left-panel">
-          <div className="logo">
-            <a onClick={this.navigateToHome}>
+  return (
+    <div className="signup-container">
+      {/* Left Panel */}
+      <div className="left-panel">
+        <div className="logo">
+          <a onClick={() => navigate('/')}>
             <img
               src={recipe_star_logo}
-              alt="InsideBox Logo"
+              alt="Recipe Star Logo"
               className="logo-img1"
             />
-            </a>
-          </div>
-          <h2 className="signup-heading">Sign up to Recipe Star</h2>
-          {error && <p className="error-text">{error}</p>}
-          {success && <p className="success-text">{success}</p>}
-          
-          <form onSubmit={this.handleSubmit} className="signup-form">
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleInputChange}
-              className="input-field"
-              placeholder="Your name"
-            />
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleInputChange}
-              className="input-field"
-              placeholder="Email address"
-            />
-            <div className="password-wrapper">
-              <input
-                type={showPassword ? 'text' : 'password'}
-                name="password"
-                value={password}
-                onChange={this.handleInputChange}
-                className="input-field"
-                placeholder="Password"
-              />
-              <span className="password-icon" onClick={this.togglePasswordVisibility}>
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
-              </span>
-            </div>
-            <button type="submit" className="signup-button">
-              Sign up
-            </button>
-          </form>
-          <p className="signin-text">
-            Have an account? <a onClick={this.navigateToLogin} className="signin-link">Sign In</a>
-          </p>
+          </a>
         </div>
+        <h2 className="signup-heading">Sign up to Recipe Star</h2>
+        {error && <p className="error-text">{error}</p>}
+        {success && <p className="success-text">{success}</p>}
 
-        {/* Right Panel */}
-        <div className="right-panel">
-        </div>
+        <form onSubmit={handleSubmit} className="signup-form">
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            className="input-field"
+            placeholder="Your name"
+          />
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            className="input-field"
+            placeholder="Email address"
+          />
+          <div className="password-wrapper">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              className="input-field"
+              placeholder="Password"
+            />
+            <span className="password-icon" onClick={togglePasswordVisibility}>
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
+          </div>
+          <button type="submit" className="signup-button">
+            Sign up
+          </button>
+        </form>
+        <p className="signin-text">
+          Have an account?{' '}
+          <a onClick={navigateToLogin} className="signin-link">
+            Sign In
+          </a>
+        </p>
       </div>
-    );
-  }
-}
+
+      {/* Right Panel */}
+      <div className="right-panel">
+        {/* Add content here if needed */}
+      </div>
+    </div>
+  );
+};
 
 export default Signup;
