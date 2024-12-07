8e6e6ea8678232b8a481151c2aa8c201170897f9
steja976 <steja976@gmail.com>
2024-12-07 00:32:29 -0500

    seven

--- src/components/Login.js
+++ src/components/Login.js
@@ -102,7 +102,7 @@ const Login = () => {
         <p className="signin-text">
           Don't Have an account?{' '}
           <a onClick={navigateToSignUp} className="signin-link">
-            Sign Upc
+            Sign Up
           </a>
         </p>
       </div>
