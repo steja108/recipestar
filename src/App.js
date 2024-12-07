3b0dd9f938f46e1ad72917e158c3970c8a54e4fc
steja976 <steja976@gmail.com>
2024-12-06 22:24:06 -0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
